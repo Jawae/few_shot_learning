720caff1972fda41587789556bdfdbf622ca1c29
hli2020 <hongyang@clarifai.com>
2018-07-18 18:00:36 -0700

    fix a bug

--- iclr18_gnn/test.py
+++ iclr18_gnn/test.py
@@ -27,28 +27,7 @@ def test_one_shot(args, model, test_samples=5000, partition='test', device='cuda
         data = loader.get_task_batch(batch_size=args.batch_size_test, n_way=args.test_N_way,
                                      num_shots=args.test_N_shots, unlabeled_extra=args.unlabeled_extra,
                                      device=device)
-        [x, labels_x_cpu, _, _, xi_s, labels_yi_cpu, oracles_yi, hidden_labels] = data
-
-        # if args.cuda:
-        #     xi_s = [batch_xi.cuda() for batch_xi in xi_s]
-        #     labels_yi = [label_yi.cuda() for label_yi in labels_yi_cpu]
-        #     oracles_yi = [oracle_yi.cuda() for oracle_yi in oracles_yi]
-        #     hidden_labels = hidden_labels.cuda()
-        #     x = x.cuda()
-        # else:
-        #     labels_yi = labels_yi_cpu
-        #
-        # xi_s = [Variable(batch_xi) for batch_xi in xi_s]
-        # labels_yi = [Variable(label_yi) for label_yi in labels_yi]
-        # oracles_yi = [Variable(oracle_yi) for oracle_yi in oracles_yi]
-        # hidden_labels = Variable(hidden_labels)
-        # x = Variable(x)
-
-        xi_s = [per_xi_s.to(device) for per_xi_s in xi_s]
-        labels_yi = [per_labels_yi.to(device) for per_labels_yi in labels_yi_cpu]
-        oracles_yi = [per_oracles_yi.to(device) for per_oracles_yi in oracles_yi]
-        hidden_labels = hidden_labels.to(device)
-        x = x.to(device)
+        [x, labels_x, _, _, xi_s, labels_yi, oracles_yi, hidden_labels] = data
 
         # Compute embedding from x and xi_s
         z = enc_nn(x)[-1]
@@ -60,7 +39,7 @@ def test_one_shot(args, model, test_samples=5000, partition='test', device='cuda
         y_pred = softmax_module.forward(output)
         y_pred = y_pred.data.cpu().numpy()
         y_pred = np.argmax(y_pred, axis=1)
-        labels_x_cpu = labels_x_cpu.numpy()
+        labels_x_cpu = labels_x.cpu().numpy()
         labels_x_cpu = np.argmax(labels_x_cpu, axis=1)
 
         for row_i in range(y_pred.shape[0]):
